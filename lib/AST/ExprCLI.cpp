c4bc460ea0d0959a12b896e01b2add759a3b35e2
João Matos <ripzonetriton@gmail.com>
2012-12-08 18:18:48 +0000

    Added files to keep CLI Expr-related AST structures.

---
+++ lib/AST/ExprCLI.cpp
@@ -0,0 +1,21 @@
+//===--- ExprCLI.cpp - (C++/CLI) Expression AST Node Implementation --------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the subclesses of Expr class declared in ExprCLI.h
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Basic/IdentifierTable.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/DeclCLI.h"
+#include "clang/AST/DeclTemplate.h"
+#include "clang/AST/ExprCLI.h"
+#include "clang/AST/TypeLoc.h"
+using namespace clang;
+
